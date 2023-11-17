9ecd05d44bd1bb2ba91b7966ba44a2c5fb6542c3
palomacha100 <palomacha100@gmail.com>
2023-11-17 14:59:04 -0300

    Conclui desafio SomaIntervalo

--- java-udemy/src/SomaIntervalo.java
+++ java-udemy/src/SomaIntervalo.java
@@ -1,5 +1,22 @@
+// Peça ao usuário para inserir dois números e calcule a soma de todos os números no
+// intervalo fechado entre esses dois números (incluindo os números inseridos).
+
+import java.util.Scanner;
+
 public class SomaIntervalo {
     public static void main (String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Digite o primeiro número:");
+        int numero1 = leitor.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int numero2 = leitor.nextInt();
+        int soma = 0;
+        for(int i = numero1; i <= numero2; i++) {
+            soma += i;
+        }
+        System.out.println("A soma dos números no intervalo de " + numero1 + " a " + numero2 + " é " + soma);
+        leitor.close();
     }
 }
